bfa579dc3da69295fcd668580539400482c5ae1d
Nicholas Turner <nick@echosec.net>
2015-02-27 13:33:41 -0800

    Added serve command for web socket server.

---
+++ src/Echosec/Socket/Commands/SocketServeCommand.php
@@ -0,0 +1,70 @@
+<?php namespace Echosec\Socket\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class LarapushServeCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'socket:serve';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Start the web socket server';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$server = new Echosec\Socket\Server\SocketServer();
+
+		$this->info('Web socket server is now listening on port ' . $this->option('port'));
+
+		$server->run($this->option('port'));
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return [
+			['port', 'p', InputOption::VALUE_OPTIONAL, 'The local port that will receive connections', 8080],
+		];
+	}
+
+}
---
+++ src/Echosec/Socket/SocketServiceProvider.php
@@ -0,0 +1,59 @@
+<?php namespace Echosec\Socket;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+
+class SocketServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Get an instance of AliasLoader
+	 * 
+	 * @return instance
+	 */
+	protected $aliasLoader;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('echosec\socket');
+
+		// Register command on boot
+		$this->commands('socket:serve');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		// IoC Command
+		$this->app['socket:serve'] = $this->app->share(function($app)
+		{
+			return new Commands\SocketServeCommand();
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}	
+
+}
